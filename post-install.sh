a5b03c173c7151d19f4c7f22d773d612d7b309ad
alcestide <angpanariti@gmail.com>
2024-03-10 11:48:36 +0100

    post-install.sh 0.1

--- post-install.sh
+++ post-install.sh
@@ -1,22 +1,30 @@
 #! /bin/bash
 # ./post-install.sh
+#     /\   | |             | | (_)   | |     
+#    /  \  | | ___ ___  ___| |_ _  __| | ___ 
+#   / /\ \ | |/ __/ _ \/ __| __| |/ _` |/ _ \
+#  / ____ \| | (_|  __/\__ \ |_| | (_| |  __/
+# /_/    \_\_|\___\___||___/\__|_|\__,_|\___|
+                                            
 echo "Post-installation script running."
 echo "---------------------------------"
 sudo pacman -S --needed base-devel git curl --noconfirm
-
+echo "Installing Yay..." && cd $HOME
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-
+echo "Installing packages listed in file..."
 yay -S - < packages
-
-if [ -d $HOME/Documents/ ]; then
-    break
-else
-    mkdir -p $HOME/Documents/
-fi
-
+[ ! -d "$HOME/Documents/" ] && mkdir -p $HOME/Documents/
+echo "Copying configuration files..."
 cp -r Wallpapers Scripts $HOME/Documents/
-sudo cp -r etc/* /etc/
+# sudo cp -r etc/* /etc/
 cp -r .config $HOME/
-cp -r .icons .themes .oh-my-zsh $HOME/
+cp -r .icons .themes .oh-my-zsh .Xdefaults .Xresources .zshrc $HOME/
+if [[ $SHELL != "/usr/bin/zsh" ]]; then
+    chsh -s /usr/bin/zsh
+    printf "Shell changed to zsh.\n" 
+else
+    printf "Your shell is already zsh.\n" 
+fi
+zsh
